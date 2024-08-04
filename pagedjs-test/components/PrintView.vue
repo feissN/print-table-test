5494c18e088aba5974658e429da72e59e60fca47
Norman <>
2024-08-04 20:34:02 +0200

    best so far

--- pagedjs-test/components/PrintView.vue
+++ pagedjs-test/components/PrintView.vue
@@ -1,120 +1,93 @@
 <template>
-  <div id="section-to-print">
-    <table>
-      <thead>
-        <tr>
-          <th>
-            <div class="head">Smile</div>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <div>
-              <h1>Cool shit</h1>
+  <table id="section-to-print">
+    <thead>
+      <tr>
+        <td>
+          <div class="header-space">
+            <h1>Header</h1>
+          </div>
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <div class="content">
+            <div class="article">
+              <h2>Main Content</h2>
 
-              <div>
-                <!-- content -->
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, consectetur.</p>
 
-                <h2>Woowie!</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, laboriosam, qui repellat sapiente iste rem natus expedita
+                tempora voluptatum sit itaque. Minima eius pariatur quam esse nostrum qui neque blanditiis eum ipsam nisi quis, hic alias
+                commodi distinctio aspernatur aliquid sapiente illum accusantium quos! Et, excepturi sapiente, corporis modi velit error,
+                sit asperiores vitae dolorem aspernatur esse ut mollitia laudantium nobis. Suscipit vel quibusdam numquam laboriosam
+                nesciunt. Dolorum vel quibusdam odit voluptatum ea repellendus, dolorem, rerum ratione suscipit numquam quidem perferendis
+                vitae consequatur saepe, esse excepturi animi totam dolor. Impedit numquam beatae molestias consectetur provident ullam
+                corrupti ipsa et aliquam, esse totam nesciunt labore est ducimus reiciendis autem exercitationem quas itaque laborum
+                possimus harum nobis. Magnam modi cum eveniet ex incidunt deserunt exercitationem explicabo doloribus fuga minus quidem
+                expedita neque officia illo sint laboriosam, ut eos corrupti est quas porro! Veniam, ut nesciunt est a atque cum ad
+                asperiores ipsum nam quaerat sapiente recusandae eligendi. Libero qui animi nobis atque dolorum corporis sequi, sunt velit
+                est quibusdam nostrum ad ullam repellat explicabo sit cupiditate commodi id numquam ducimus harum possimus pariatur
+                obcaecati! Possimus, labore dolorum provident nam tempore illum nostrum? Nesciunt, provident similique. Nobis repellendus
+                voluptatem tenetur, perspiciatis a sunt, nemo magnam enim ducimus nulla possimus, officiis aperiam. Dolores minima vitae
+                hic! Officia inventore a placeat, quas facere dignissimos veniam aliquam delectus impedit eum! Sequi, et. Praesentium odio
+                quaerat dignissimos qui soluta cum, molestias vitae magni autem sint magnam possimus eos architecto explicabo. Dolorum quas
+                eos nulla dicta ea consequuntur architecto ab et impedit dolorem. Aperiam ipsam qui sint asperiores? Maiores beatae commodi
+                aspernatur. Quisquam harum labore voluptate. Velit, laboriosam voluptate dolorum maiores reiciendis soluta maxime esse,
+                tempore, assumenda molestiae consequatur et quo aperiam dolor est laborum voluptatem eos nesciunt impedit animi consectetur
+                omnis ratione. Voluptatem non aperiam pariatur eligendi.
+              </p>
 
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem voluptas iste doloribus quidem sequi consequuntur
-                  aspernatur ipsum sed voluptatibus modi error, provident consequatur et qui delectus perspiciatis eligendi quisquam!
-                  Eveniet porro necessitatibus eligendi eaque voluptas accusamus maiores sequi alias? Voluptatem alias aliquid, debitis sed
-                  eveniet quis modi dicta porro dignissimos voluptatibus sapiente sequi ipsam velit consequatur eius corporis vel a
-                  reprehenderit consectetur nobis amet? Fugiat veritatis vel expedita totam obcaecati esse, beatae ad eveniet, quae, magnam
-                  minus libero qui ipsa neque id itaque molestiae. Aut recusandae iure ratione aliquam autem similique iste, molestiae enim
-                  iusto at alias necessitatibus. Fugit, tenetur nam consequatur assumenda iste accusamus dicta tempora aut perferendis
-                  rerum, sunt repellendus quis corporis a molestias dolorum rem quibusdam quia odit quisquam. Quae in unde sint quasi, ut
-                  temporibus, nostrum reiciendis mollitia laboriosam maiores voluptate nam provident ipsum ratione neque ullam natus
-                  corporis eos, dolore quisquam culpa est optio dolor. Officiis labore rem cum repudiandae ad hic obcaecati soluta omnis
-                  velit voluptatum, aperiam dolorum quis necessitatibus inventore explicabo ipsum. Odit cumque modi possimus necessitatibus,
-                  tempore dolore? Odio, amet? Fuga sapiente totam eaque necessitatibus quod? Aliquam possimus, explicabo a adipisci dolore
-                  deserunt magni totam fugiat tempora excepturi nulla vel consectetur vitae!
-                </p>
-
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem voluptas iste doloribus quidem sequi consequuntur
-                  aspernatur ipsum sed voluptatibus modi error, provident consequatur et qui delectus perspiciatis eligendi quisquam!
-                  Eveniet porro necessitatibus eligendi eaque voluptas accusamus maiores sequi alias? Voluptatem alias aliquid, debitis sed
-                  eveniet quis modi dicta porro dignissimos voluptatibus sapiente sequi ipsam velit consequatur eius corporis vel a
-                  reprehenderit consectetur nobis amet? Fugiat veritatis vel expedita totam obcaecati esse, beatae ad eveniet, quae, magnam
-                  minus libero qui ipsa neque id itaque molestiae. Aut recusandae iure ratione aliquam autem similique iste, molestiae enim
-                  iusto at alias necessitatibus. Fugit, tenetur nam consequatur assumenda iste accusamus dicta tempora aut perferendis
-                  rerum, sunt repellendus quis corporis a molestias dolorum rem quibusdam quia odit quisquam. Quae in unde sint quasi, ut
-                  temporibus, nostrum reiciendis mollitia laboriosam maiores voluptate nam provident ipsum ratione neque ullam natus
-                  corporis eos, dolore quisquam culpa est optio dolor. Officiis labore rem cum repudiandae ad hic obcaecati soluta omnis
-                  velit voluptatum, aperiam dolorum quis necessitatibus inventore explicabo ipsum. Odit cumque modi possimus necessitatibus,
-                  tempore dolore? Odio, amet? Fuga sapiente totam eaque necessitatibus quod? Aliquam possimus, explicabo a adipisci dolore
-                  deserunt magni totam fugiat tempora excepturi nulla vel consectetur vitae!
-                </p>
-
-                <h2 class="pagebreak">Ich mach nen Page-Break!</h2>
-
-                <table class="inner">
-                  <tr>
-                    <th>Pos</th>
-                    <th>Name</th>
-                    <th>Preis</th>
-                  </tr>
-                  <tr v-for="i in 30" :key="i">
-                    <td>{{ i }}</td>
-                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, officia.</td>
-                    <td>100</td>
-                  </tr>
-                </table>
-
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente sequi repellat voluptas, aperiam, magnam iure aliquam,
-                  rerum quam quidem ipsa harum a molestiae beatae dolorem atque blanditiis tempore. Repellendus et recusandae aperiam eum
-                  obcaecati nisi vel accusamus modi animi fuga quidem corporis ipsam dolorum consequatur, perspiciatis autem cumque rem
-                  unde.
-                </p>
-
-                <table class="inner">
-                  <tr>
-                    <th>Pos</th>
-                    <th>Name</th>
-                    <th>Preis</th>
-                  </tr>
-                  <tr v-for="i in 30" :key="i">
-                    <td>{{ i }}</td>
-                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, officia.</td>
-                    <td>100</td>
-                  </tr>
-                </table>
-
-                <p>Und nun zum abschluss</p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis doloremque blanditiis, incidunt, quibusdam voluptates
-                  quae facere architecto quo minima expedita corporis. Recusandae doloribus voluptatum fuga quos, odit suscipit ex a.
-                  Aliquid voluptates consequatur, adipisci reprehenderit voluptas quisquam nostrum ea alias illum, soluta molestias
-                  similique et. Voluptate animi obcaecati corrupti est sed sequi ea cum esse, totam perferendis minima praesentium vel
-                  aliquam dolore facere asperiores nemo, sapiente modi quam? Explicabo iste ipsa mollitia dolorum culpa quas? Beatae numquam
-                  non minima omnis! Sit inventore vel repudiandae debitis excepturi quo placeat, incidunt odit animi! Ipsa recusandae
-                  temporibus doloremque repellendus optio corporis quos esse!
-                </p>
-              </div>
+              <div class="pagebreak"></div>
             </div>
-          </td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>
-            <div class="foot">
-              <div class="sticky">Copyright stuff</div>
+
+            <div class="article">
+              <table>
+                <tr>
+                  <th>Pos</th>
+                  <th>Name</th>
+                  <th>Preis</th>
+                </tr>
+                <tr v-for="i in 60" :key="i">
+                  <td>{{ `${i}`.padStart(2, "0") }}</td>
+                  <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, quis?</td>
+                  <td>{{ formatEuro(Math.round(Math.random() * 10000)) }}</td>
+                </tr>
+              </table>
             </div>
-          </td>
-        </tr>
-      </tfoot>
-    </table>
-  </div>
+
+            <div class="article">
+              <h2>Schlusstext</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, inventore atque beatae illum perferendis porro aperiam,
+                facere officiis, neque alias doloribus quibusdam blanditiis ducimus debitis. Ea, explicabo, magnam veniam est, voluptatibus
+                beatae corporis quas placeat quos reiciendis accusamus eaque inventore consequatur blanditiis? Aut, dolores exercitationem
+                fugiat id ea deserunt aspernatur!
+              </p>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td>
+          <div class="footer-space">
+            <div class="footer-content">
+              <h3>Footer auf jeder Seite</h3>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </tfoot>
+  </table>
 </template>
 
 <script setup lang="ts">
+const formatEuro = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format;
+
 const handlePrint = () => {
   window.print();
 };
@@ -127,43 +100,55 @@ defineExpose({
 <style>
 @page {
   size: A4;
-  margin: 11mm 17mm 17mm 17mm;
+  /* margin: 11mm 17mm 17mm 17mm; */
+
+  margin-bottom: 10cm;
 }
 
 #section-to-print {
   width: 210mm;
   min-height: 297mm;
   border: 1px solid black;
-  /* padding-left: 2cm;
-  padding-right: 2cm; */
-  padding-top: 10mm;
-  padding-right: 20mm;
-  padding-left: 15mm;
-  padding-bottom: 2cm;
-}
-
-.head {
-  padding-top: 2cm;
-  padding-bottom: 2cm;
-}
-
-.sticky {
-  padding-bottom: 2cm;
-  padding-top: 2cm;
-}
-
-.inner tr {
-  background-color: chartreuse;
+  padding-top: 11mm;
+  padding-right: 17mm;
+  padding-bottom: 17mm;
+  padding-left: 17mm;
 }
 
 .pagebreak {
-  border-bottom: 1px dashed black;
+  border-bottom: dashed 1px black;
+  margin-bottom: 1rem;
+  position: relative;
+  opacity: 50%;
 }
-tfoot {
-  display: table-footer-group;
+
+.pagebreak::after {
+  content: "Seitenumbruch";
+  position: relative;
+  display: inline-block;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.header-space,
+.footer-space,
+.footer-content {
+  height: 100px;
+}
+
+.footer-content {
+  border-top: 1px solid black;
+  width: 100%;
 }
 
 @media print {
+  thead {
+    display: table-header-group;
+  }
+  tfoot {
+    display: table-footer-group;
+  }
+
   .sticky {
     position: fixed;
     bottom: 0;
@@ -174,9 +159,9 @@ tfoot {
   }
 
   .pagebreak {
-    clear: both;
-    page-break-after: always;
+    page-break-before: always;
     border-bottom: none;
+    visibility: hidden;
   }
 
   #section-to-print {
@@ -187,6 +172,12 @@ tfoot {
     print-color-adjust: exact;
     padding-bottom: 4rem;
     border: none;
+  }
+
+  .footer-content {
+    position: fixed;
+    bottom: 0;
+    width: calc(100% - 17mm * 2);
   }
 }
 </style>
